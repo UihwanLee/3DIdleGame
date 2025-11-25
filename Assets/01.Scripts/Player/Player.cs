43133333884de1c8aaaf91d3018e2ef48fb470ff
UihwanLee <luh5063014@naver.com>
2025-11-25 11:36:23 +0900

    feat: Monster를 찾아서 AI Navigation을 이용 탐색해서 찾아가도록 구현

--- Assets/01.Scripts/Player/Player.cs
+++ Assets/01.Scripts/Player/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
@@ -11,15 +12,21 @@ public class Player : MonoBehaviour
 
     public Animator Animator { get; private set; }
 
+    [field:Header("AI")]
+    [field:SerializeField] public NavMeshAgent Agent { get; private set; }
+
     private PlayerStateMachine _stateMachine;
+
 
     private void Awake()
     {
         AnimationData.Initialize();
         Animator = GetComponentInChildren<Animator>();
 
+        Agent = GetComponent<NavMeshAgent>();
+
         _stateMachine = new PlayerStateMachine(this);
-        _stateMachine.ChangeState(_stateMachine.IdleState);
+        _stateMachine.ChangeState(_stateMachine.RunState);
     }
 
     private void Update()
--- Assets/01.Scripts/Player/PlayerAutoFindState.cs
+++ Assets/01.Scripts/Player/PlayerAutoFindState.cs
@@ -2,20 +2,50 @@ using UnityEngine;
 
 public class PlayerAutoFindState : PlayerBaseState
 {
+    private Transform target;
+    private bool isMovingToTarget = false;
+
     public PlayerAutoFindState(PlayerStateMachine stateMachine) : base(stateMachine)
     {
     }
 
     public override void Enter()
     {
+        isMovingToTarget = false;
+
         base.Enter();
         StartAnimation(stateMachine.Player.AnimationData.AutoFindParameterHash);
+        SetTargetMonster();
+        MoveToTarget();
+    }
+
+    private void SetTargetMonster()
+    {
+        target = null;
+
+        // 근방의 몬스터를 찾아서 Destination 찾기
+        Monster monster = GameObject.FindFirstObjectByType<Monster>();
+        if (monster != null)
+        {
+            target = monster.transform;
+        }
+    }
+
+    private void MoveToTarget()
+    {
+        // 목표 설정 및 이동
+        if(target != null)
+        {
+            stateMachine.Player.Agent.SetDestination(target.position);
+            isMovingToTarget = true;
+        }
     }
 
     public override void Exit()
     {
         base.Exit();
         StopAnimation(stateMachine.Player.AnimationData.AutoFindParameterHash);
+        isMovingToTarget = false;
     }
 
     public override void Update()
