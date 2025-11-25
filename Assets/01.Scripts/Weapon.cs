cb9f37234cae4c428db80ef19553dda2eeddcbb5
UihwanLee <luh5063014@naver.com>
2025-11-25 21:39:43 +0900

    feat: Weapon 구현, Player 전용으로 칼 Collider 확인 요구됨

---
+++ Assets/01.Scripts/Weapon.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Collider myCollider;
+
+    private int _damage;
+
+    private List<Collider> alreadyCollider = new List<Collider>();
+
+    private void OnEnable()
+    {
+        alreadyCollider.Clear();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == myCollider) return;
+        if (alreadyCollider.Contains(other)) return;
+
+        alreadyCollider.Add(other);
+
+        if(other.TryGetComponent(out Health health))
+        {
+            health.TakeDamage(_damage);
+        }
+    }
+
+    public void SetAttack(int damage)
+    {
+        this._damage = damage;
+    }
+}
