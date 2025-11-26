f588ff21a9e42f2994e22c779dc172378540aefd
UihwanLee <luh5063014@naver.com>
2025-11-26 20:52:24 +0900

    feat: MapGenerator 스크립트 생성

---
+++ Assets/01.Scripts/Map/MapGenerator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 맵을 자동 생성하는 스크립트
+/// </summary>
+public class MapGenerator : MonoBehaviour
+{
+    
+}
