2e22c2dd8fe8d18ed453d2ce1c0df33374c648b4
UihwanLee <luh5063014@naver.com>
2025-11-26 09:21:50 +0900

    chore: Monster 스크립트 일부 수정

--- Assets/01.Scripts/Monsters/MonsterStateMachine.cs
+++ Assets/01.Scripts/Monsters/MonsterStateMachine.cs
@@ -7,7 +7,7 @@ public class MonsterStateMachine : StateMachine
     public Monster Monster { get; }
 
 
-    public GameObject Target { get; private set; }
+    public Health Target { get; private set; }
     public MonsterIdleState IdleState { get; }
     public MonsterAttackState AttackState { get; }
     public MonsterChasingState ChasingState { get; }
@@ -18,7 +18,7 @@ public class MonsterStateMachine : StateMachine
     public MonsterStateMachine(Monster monster)
     {
         this.Monster = monster;
-        Target = GameObject.FindGameObjectWithTag("Player");
+        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
 
         IdleState = new MonsterIdleState(this);
         AttackState = new MonsterAttackState(this);
--- Assets/01.Scripts/Monsters/State/03.AttackModeState/MonsterAttackState.cs
+++ Assets/01.Scripts/Monsters/State/03.AttackModeState/MonsterAttackState.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MonsterAttackState : MonsterAttackModeState
 {
+    private bool _alreadyAppliedDealing;
+
     public MonsterAttackState(MonsterStateMachine stateMachine) : base(stateMachine)
     {
     }
@@ -12,6 +14,8 @@ public class MonsterAttackState : MonsterAttackModeState
     {
         StartAnimation(stateMachine.Monster.AnimationData.AttackModeParameterHash);
         StartAnimation(stateMachine.Monster.AnimationData.AttackParameterHash);
+
+        _alreadyAppliedDealing = false;
     }
 
     public override void Exit()
@@ -26,7 +30,20 @@ public class MonsterAttackState : MonsterAttackModeState
 
         float normalizedTime = GetNormalizedTime(stateMachine.Monster.Animator, "Attack");
 
-        if (normalizedTime > 1f)
+
+        if (normalizedTime < 1f)
+        {
+            if(!_alreadyAppliedDealing && normalizedTime >= stateMachine.Monster.Data.Dealing_Start_TransitionTime)
+            {
+                _alreadyAppliedDealing = true;
+            }
+
+            if(_alreadyAppliedDealing && normalizedTime >= stateMachine.Monster.Data.Dealing_End_TransitionTime)
+            {
+
+            }
+        }
+        else
         {
             stateMachine.IsAttacking = false;
             stateMachine.ChangeState(stateMachine.AttackIdleState);
