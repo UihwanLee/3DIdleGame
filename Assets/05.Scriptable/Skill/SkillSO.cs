a5429980b281eddf241a80467ec4f9ef27b402ec
UihwanLee <luh5063014@naver.com>
2025-11-26 14:38:34 +0900

    feat: SkillSO 구현 / Skill 모두 계수로 계산하여 BaseState에 계수를 곱하여 계산한다.

---
+++ Assets/05.Scriptable/Skill/SkillSO.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// SkillType - Active / Passive가 아닌
+// 공격 가능 / Buff로 나눔
+public enum SkillTpye
+{
+    Active,
+    Buff,
+}
+
+public enum BuffType
+{
+    SpeedUp,
+    Heal,
+}
+
+// Active / Buff 모두 계수로 계산
+
+[Serializable]
+public class ActvieInfo
+{
+    [field: SerializeField][field: Range(1f, 3f)] public int DamageMultiplier { get; private set; }
+    [field: SerializeField][field: Range(0f, 1f)] public float Dealing_Start_TransitionTime { get; private set; }
+    [field: SerializeField][field: Range(0f, 1f)] public float Dealing_End_TransitionTime { get; private set; }
+}
+
+[Serializable]
+public class BuffInfo
+{
+    [field: SerializeField][field: Range(1f, 2f)] public float ValueMultiplier { get; private set; }
+    [field: SerializeField] public BuffType Type { get; private set; }
+}
+
+[CreateAssetMenu(fileName = "Skill", menuName = "Active/Skill")]
+public class SkillSO : ScriptableObject
+{
+    [field: SerializeField] public string SkillName { get; private set; }
+    [field: SerializeField] public SkillTpye Type { get; private set; }
+    [field: SerializeField] public ActvieInfo ActvieInfo { get; private set; }
+    [field: SerializeField] public BuffInfo BuffInfo { get; private set; }
+}
