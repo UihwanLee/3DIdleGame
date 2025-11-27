9310caf5c51ed37674ce171b719cb045d703fd81
UihwanLee <luh5063014@naver.com>
2025-11-27 10:58:33 +0900

    feat: Item / ItemSO 구현 ItemSO에 데이터를 덮어쓰지 않도록 ItemData 스크립트 활용

---
+++ Assets/01.Scripts/Item/ItemData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : MonoBehaviour
+{
+    public ItemSO data;
+
+    public int enhacneLevel;
+}
---
+++ Assets/05.Scriptable/Item/ItemSO.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Equipalbe,
+    Consumable,
+    Resource
+}
+
+public enum ConsumableType
+{
+    Health,
+    Speed
+}
+
+[Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "Item")]
+public class ItemSO : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+}
